7ef96ab07504db40d31564ecc5837ce372a6814e
S4M73l09 <sam1999xxx@gmail.com>
2026-04-18 16:56:42 +0200

    Policy - Checkov folder added in this repo/branch

---
+++ environments/staging/checkov/terraform/check_firewall_no_ssh_open.py
@@ -0,0 +1,34 @@
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckCategories, CheckResult
+
+class FirewallNoSSHOpen(BaseResourceCheck):
+    def __init__(self) -> None:
+        name = "Firewall must not allow SSH from 0.0.0.0/0"
+        id = "CKV2_GCP_CUSTOM_3"
+        supported_resources = ["google_compute_firewall"]
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: dict) -> CheckResult:
+        direction = conf.get("direction", ["INGRESS"])[0]
+        if direction != "INGRESS":
+            return CheckResult.PASSED
+
+        source_ranges = conf.get("source_ranges", [])
+        flat_ranges = source_ranges[0] if source_ranges and isinstance(source_ranges[0], list) else source_ranges
+        if "0.0.0.0/0" not in flat_ranges:
+            return CheckResult.PASSED
+
+        allow_blocks = conf.get("allow", [])
+        if allow_blocks and isinstance(allow_blocks[0], list):
+            allow_blocks = allow_blocks[0]
+
+        for allow in allow_blocks:
+            ports = allow.get("ports", [])
+            flat_ports = ports[0] if ports and isinstance(ports[0], list) else ports
+            if "22" in flat_ports:
+                return CheckResult.FAILED
+
+        return CheckResult.PASSED
+
+check = FirewallNoSSHOpen()
