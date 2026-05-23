6ee87befd614f89bf3afe4244b5d6b32bda2f2f4
S4M73l09 <sam1999xxx@gmail.com>
2026-05-23 15:40:42 +0200

    change state prefix in environments/global

--- environments/global/backend.tf
+++ environments/global/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "bootstrap-476212-tfstate" # <-- tu bucket
-    prefix = "live/global"              # carpeta separada para este estado
+    prefix = "live/dev/global"              # carpeta separada para este estado
   }
 }
